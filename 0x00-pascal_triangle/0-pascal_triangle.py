49985e121aee85b377119af6409677b14309fee4
jeniffermukami <mukamijeniffer6@gmail.com>
2024-03-26 02:49:21 +0300

    m

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,37 +1,49 @@
 #!/usr/bin/python3
 """
-0-main
+This module contains the function 'pascal_triangle' which generates
+Pascal's triangle up to 'n' levels. It includes a 'print_triangle'
+function to display the triangle.
 """
+
+
 def pascal_triangle(n):
     """
     Generate Pascal's triangle of height n.
 
     Parameters:
-    - n (int): The height of the triangle.
+    n (int): The height of the triangle.
 
     Returns:
-    - List[List[int]]: A list of lists of integers representing Pascal's triangle.
+    List[List[int]]: A list of lists of integers representing Pascal's triangle.
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
+
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            # Ensure line length does not exceed 79 characters
+            sum_pair = triangle[i-1][j-1] + triangle[i-1][j]
+            row.append(sum_pair)
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
+
 
 def print_triangle(triangle):
     """
-    Print the triangle
+    Print the triangle.
+
+    Parameters:
+    triangle (list of lists of ints): The Pascal's triangle to print.
     """
     for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+        # Use f-string for improved readability
+        print(f"[{','.join([str(x) for x in row])}]")
+
 
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
